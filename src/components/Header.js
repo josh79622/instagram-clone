5ee53f88a445da895d1c533fded5fbf2748a0280
Josh Tsai <b98705028@gmail.com>
2023-01-25 19:11:55 +0800

    add search bar to the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,37 @@
 import Image from 'next/image'
 import React from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 export default function Header() {
-  return (
-    <div>
+  return (  
+    <div className="flex items-center justify-between max-w-6xl">
       {/* Logo */}
-      <div className="flex items-center justify-between max-w-6xl">
-        <div className=" cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
-            layout='fill'
-            className="object-contain"
-            alt="LOGO"
-          />
-        </div>
-        <div className=" cursor-pointer h-24 w-10 relative lg:hidden">
-          <Image
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1200px-Instagram-Icon.png"
-            layout='fill'
-            className="object-contain"
-            alt="LOGO"
-          />
-        </div>
-        <div className="">Right</div>
+      <div className=" cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png"
+          layout='fill'
+          className="object-contain"
+          alt="LOGO"
+        />
+      </div>
+      <div className=" cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Instagram-Icon.png/1200px-Instagram-Icon.png"
+          layout='fill'
+          className="object-contain"
+          alt="LOGO"
+        />
       </div>
       {/* Search */}
-
+      <div className="relative">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className='w-5 text-gray-500'/>
+        </div>
+        <input type="text" className=" pl-10 border-gray-500 bg-gray-50 text-sm focus:ring-black focus:border-black rounded-md" placeholder='Search'/>
+      </div>
       {/* Menu */}
+      <div className="">Right</div>
     </div>
+      
   )
 }
