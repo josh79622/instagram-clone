9aaf376afb078e170eee5a0a290bb545a79fa728
Josh Tsai <b98705028@gmail.com>
2023-01-26 21:25:02 +0800

    add caption and input box to the post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { EllipsisHorizontalIcon } from '@heroicons/react/24/solid'
-import { HeartIcon, ChatBubbleOvalLeftEllipsisIcon, BookmarkIcon } from '@heroicons/react/24/outline'
+import { HeartIcon, ChatBubbleOvalLeftEllipsisIcon, BookmarkIcon, FaceSmileIcon } from '@heroicons/react/24/outline'
 
 export default function Post({id, username, userImage, images, caption}) {
   return (
@@ -23,6 +23,20 @@ export default function Post({id, username, userImage, images, caption}) {
         </div>
         <BookmarkIcon class="btn"/>
       </div>
+
+      {/* Post Caption */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+      {/* Post Comments */}
+
+      {/* Post Input Box */}
+      <form action="" className="flex items-center p-4">
+        <FaceSmileIcon className="h-7"/>
+        <input className="border-none flex-1 focus:ring-0" type="text" placeholder="enter your comment"/>
+        <button className="text-blue-400 font-bold">Post</button>
+      </form>
     </div>
   )
 }
